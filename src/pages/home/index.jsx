54ee8d5e1374090ad48fce8536975e06dd2dc127
Lucas Alves <lc_2014@hotmai.com>
2022-07-20 11:07:31 -0300

    fix home screen

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,31 +1,9 @@
 import { NavBar } from '../../components/navBar';
-import { CustomBox as Box } from '../../components/box';
-import { Title, Container, Main, Aside } from './styles';
 
 function Home() {
   return (
     <>
       <NavBar />
-      <Container>
-        <Box height={'300px'} width={'100%'}>
-          <Title>Header</Title>
-        </Box>
-      </Container>
-      <Container>
-        <Main>
-          <Box height={'200px'}>
-            <Title>Home</Title>
-          </Box>
-          <Box height={'200px'}>
-            <Title>publicação</Title>
-          </Box>
-        </Main>
-        <Aside>
-          <Box height={'200px'}>
-            <Title>Ultimas noticias</Title>
-          </Box>
-        </Aside>
-      </Container>
     </>
   );
 }
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -4,20 +4,3 @@ export const Title = styled.h1`
   font-size: 30px;
   color: #ffffff;
 `;
-
-export const Container = styled.div`
-  max-width: 900px;
-  min-width: 300px;
-  margin: 0 auto;
-  display: flex;
-  justify-content: center;
-`;
-
-export const Main = styled.div`
-  width: 600px;
-`;
-
-export const Aside = styled.aside`
-  width: 300px;
-  margin-left: 10px;
-`;
