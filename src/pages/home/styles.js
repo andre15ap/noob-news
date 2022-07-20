b6cad4201283eb5a5222288b103df6acb54e6bdf
Lucas Alves <lc_2014@hotmai.com>
2022-07-20 11:44:27 -0300

    Revert "fix home screen"
    This reverts commit 54ee8d5e1374090ad48fce8536975e06dd2dc127.

--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -4,3 +4,20 @@ export const Title = styled.h1`
   font-size: 30px;
   color: #ffffff;
 `;
+
+export const Container = styled.div`
+  max-width: 900px;
+  min-width: 300px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Main = styled.div`
+  width: 600px;
+`;
+
+export const Aside = styled.aside`
+  width: 300px;
+  margin-left: 10px;
+`;
